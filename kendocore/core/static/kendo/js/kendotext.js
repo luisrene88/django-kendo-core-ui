bcb108598112e10e9c5044a88a73c5858354b20d
luisrene88 <luisd162000@yahoo.com.mx>
2016-06-22 18:19:51 -0700

    aver si funciona asi

--- kendocore/core/static/kendo/js/kendotext.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-	$(':input[type=text]').each(function(index, value){
-      var id = $(value).attr('id')
-			var idf = "#"+id;
-      $(idf).addClass('k-textbox');
-	});
-
-});
